0ac79b849af904def1ff180d43da4920f5cb52d3
Ubuntu <ubuntu@ip-172-31-28-196.ap-northeast-2.compute.internal>
2018-11-23 23:33:31 +0900

    added esp32

---
+++ esp32/install.sh
@@ -0,0 +1,27 @@
+
+TOOLCHAIN='xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz'
+ESP32_PATH=~/esp32
+IDF_PATH=~/esp32/esp-idf
+
+# Per install
+sudo apt-get install gcc git wget make libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-cryptography python-future
+
+# Toolchain
+wget https://dl.espressif.com/dl/$TOOLCHAIN
+mkdir -p $ESP32_PATH
+tar -xzf $TOOLCHAIN -C ${ESP32_PATH}
+
+echo '[OK] Installed ESP32 Toolchain'
+echo 'export PATH="$HOME/esp/xtensa-esp32-elf/bin:$PATH"' >> ~/.bashrc
+echo '. ~/.bachrc'
+
+# End
+rm $TOOLCHAIN
+
+cd $ESP32_PATH
+git clone --recursive https://github.com/espressif/esp-idf.git
+echo '[OK] Installed ESP32 IDF'
+echo 'export IDF_PATH=~/esp/esp-idf' >> ~/.bashrc
+echo '. ~/.bachrc'
+
+python -m pip install --user -r $IDF_PATH/requirements.txt
